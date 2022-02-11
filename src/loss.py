d772f32fefad1694eb77242bf93950b87e4643cc
LebowskiSama <anushvenkat101@gmail.com>
2022-02-11 17:07:32 +0000

    fixed NaN loss

--- src/loss.py
+++ src/loss.py
@@ -1,7 +1,8 @@
 import torch
 import torch.nn as nn
+import config
 
-from utils import intersection_over_union
+from utils import int_over_union
 
 
 class YOLOLoss(nn.Module):
@@ -14,9 +15,9 @@ class YOLOLoss(nn.Module):
         # self.entropy = nn.CrossEntropyLoss()
         self.sigmoid = nn.Sigmoid()
         # loss weights range 1 -> 10
-        self.lambda_noobj = 10
-        self.lambda_obj = 1
-        self.lambda_box = 10
+        # self.lambda_noobj = 10
+        # self.lambda_obj = 1
+        # self.lambda_box = 10
 
     def forward(self, predictions, target, anchors):
         # No object loss
@@ -27,23 +28,26 @@ class YOLOLoss(nn.Module):
         no_object_loss = self.bce(
             (predictions[..., 0:1][noobj]), (target[..., 0:1][noobj])
         )
+        if no_object_loss == torch.tensor(torch.nan):
+            no_object_loss = config.EPS
 
-        # Object loss
-        # More nuanced to use iou as a factor during loss calculation
-        anchors = anchors.reshape(1, 3, 1, 1, 2) # Comes in as (3 x 2), reshaped to 1, 3, 1, 1, 2 for computations
-        box_preds = torch.cat([self.sigmoid(predictions[..., 1:3]), torch.exp(predictions[..., 3:5]) * anchors], dim=-1)
-        ious = intersection_over_union(box_preds[obj], target[..., 1:5][obj]).detach()
-        object_loss = self.bce(
-            (predictions[..., 0:1][obj]), (ious * target[..., 0:1][obj])
-        )
+        # Custom check to see if targets actually have any labels
+        obj_presence = len(target[obj])
+        if obj_presence != 0:
+            # Object loss
+            # More nuanced to use iou as a factor during loss calculation
+            anchors = anchors.reshape(1, 3, 1, 1, 2)
+            box_preds = torch.cat([self.sigmoid(predictions[..., 1:3]), torch.exp(predictions[..., 3:5]) * anchors], dim=-1)
+            ious = int_over_union(box_preds[obj], target[..., 1:5][obj]).detach()
+            object_loss = self.mse(
+                self.sigmoid(predictions[..., 0:1][obj]), (ious * target[..., 0:1][obj])
+            )
 
-        # Box coordinates loss
-        predictions[..., 1:3] = self.sigmoid(predictions[..., 1:3])
-        target[..., 3:5] = torch.log((1e-16 + target[..., 3:5] / anchors))
-        box_loss = self.mse(predictions[..., 1:5][obj], target[..., 1:5][obj])
+            # Box coordinates loss
+            predictions[..., 1:3] = self.sigmoid(predictions[..., 1:3])
+            target[..., 3:5] = torch.log((1e-16 + target[..., 3:5] / anchors))
+            box_loss = self.mse(predictions[..., 1:5][obj], target[..., 1:5][obj])
 
-        return (
-            self.lambda_noobj * no_object_loss
-            + self.lambda_obj * object_loss
-            + self.lambda_box * box_loss
-        )
+            return no_object_loss + object_loss + box_loss
+
+        return no_object_loss
